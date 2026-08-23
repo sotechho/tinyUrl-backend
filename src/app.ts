4614d5ec15dbbd5ea37cb0920ff1efea488ecb6c
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-08-23 20:53:33 +0300

    feat: add express app setup with middleware and 404 error handling

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { type Request, type Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+
+const app = express();
+
+// MIDDLEWARES
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+
+// NOTFOUND
+app.use((req: Request, res: Response) => {
+  const url = req.originalUrl;
+  const message = 'The path you are trying to access does not exist';
+
+  return res.status(404).json({
+    message,
+    errors: [{ path: url, message }],
+  });
+});
+
+// ERRORS
+export default app;
