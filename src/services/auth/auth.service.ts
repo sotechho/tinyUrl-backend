0ffec0af3b6fd9cb0d8d9aad9a33cae6b42b34ed
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-09-01 08:30:18 +0300

    refactor: improve error handling and code structure in user registration and add send email verification

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,38 +1,61 @@
 import { db } from '@/db';
 import { usersTable } from '@/db/tables';
-import { ConflictError } from '@/utils';
+import { AppError, ConflictError } from '@/utils';
+import logger from '@/utils/logger';
 import type { RegisterInput } from '@/validators';
 import { eq } from 'drizzle-orm';
+import { mailService } from '../notifications';
 import { passwordService } from './password.service';
 import { verificationService } from './verification.service';
 
 class AuthService {
   async register(data: RegisterInput): Promise<void> {
-    const normalizedEmail = data.email.trim().toLowerCase();
-    // check if user exists
-    const userExists = await db
-      .select()
-      .from(usersTable)
-      .where(eq(usersTable.email, normalizedEmail));
+    try {
+      // normalize email
+      const normalizedEmail = data.email.trim().toLowerCase();
+      // check if user exists
+      const userExists = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.email, normalizedEmail));
 
-    if (userExists.length > 0) {
-      throw new ConflictError('User is already exists');
+      // throw conflict error
+      if (userExists.length > 0) {
+        throw new ConflictError('User is already exists');
+      }
+      // hash the password
+      const hashedPassword = await passwordService.hashPassword(data.password);
+      // verification token and expires
+      const emailVerificationToken = verificationService.generateToken();
+      const emailVerificationExpires =
+        verificationService.generateTokenExpiration();
+      // username extract
+      const username = (data.username ||
+        normalizedEmail.split('@')[0]) as string;
+
+      // insert to the database
+      await db.insert(usersTable).values({
+        email: normalizedEmail,
+        username,
+        password: hashedPassword,
+        emailVerificationToken,
+        emailVerificationExpires,
+      });
+      // send verification mail
+      await mailService.sendVerificationEmail(normalizedEmail, {
+        verificationToken: emailVerificationToken,
+        name: username,
+      });
+    } catch (error: any) {
+      logger.error('Failed user registeration', {
+        ...error,
+      });
+
+      if (error instanceof AppError) {
+        throw error;
+      }
+      throw new AppError('Failed user registeration');
     }
-
-    const hashedPassword = await passwordService.hashPassword(data.password);
-    const emailVerificationToken = verificationService.generateToken();
-    const emailVerificationExpires =
-      verificationService.generateTokenExpiration();
-
-    const username = data.username || normalizedEmail.split('@')[0];
-
-    await db.insert(usersTable).values({
-      email: normalizedEmail,
-      username,
-      password: hashedPassword,
-      emailVerificationToken,
-      emailVerificationExpires,
-    });
   }
 }
 
